228572993112d82e7aa5fae2a6ad437fb91bc666
Stevie McCullough <stephen.mccullough@gmail.com>
2022-12-05 02:07:13 +0000

    Installed tailwind css

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+  
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
